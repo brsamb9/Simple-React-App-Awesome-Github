0d6bef72472d9bfc01470c6556c170f33e648a55
brsamb9 <69250411+brsamb9@users.noreply.github.com>
2020-11-04 15:24:54 +0000

    Delete routes.py

--- backend_old/app/resources/routes.py
+++
@@ -1,9 +0,0 @@
-from .awesomegit import AwesomeGitsApi, AwesomeGitApi
-from .auth import SignupApi, LoginApi
-
-def initialize_routes(api):
-    api.add_resource(AwesomeGitsApi, '/api/awesomegits')
-    api.add_resource(AwesomeGitApi, '/api/awesomegits/<id>')
-
-    api.add_resource(SignupApi, '/api/auth/signup')
-    api.add_resource(LoginApi, '/api/auth/login')
