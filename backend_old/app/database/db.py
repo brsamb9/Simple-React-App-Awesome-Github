37483eb946ccf9f5a605a730b1c498fcacd8f4a4
brsamb9 <69250411+brsamb9@users.noreply.github.com>
2020-11-04 15:26:36 +0000

    Delete db.py

--- backend_old/app/database/db.py
+++
@@ -1,7 +0,0 @@
-# https://dev.to/paurakhsharma/flask-rest-api-part-1-using-mongodb-with-flask-3g7d
-from flask_mongoengine import MongoEngine
-
-db = MongoEngine()
-
-def initialize_db(app):
-    db.init_app(app)
